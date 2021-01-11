500260c2d4b99d1d080cc030ed399087d4f4fde2
mdumbu <moses@dumbuya.de>
2021-01-11 23:03:17 +0100

    A program demonstrating the concept of inner classes.

---
+++ InnerClass/src/de/dumbuya/Button.java
@@ -0,0 +1,27 @@
+package de.dumbuya;
+
+public class Button {
+    String title;
+    private OnClickListener onClickListener;
+
+    public Button(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
+
+    public void onclick() {
+        this.onClickListener.onClick
+                (this.title);
+    }
+
+    public interface OnClickListener{
+        public void onClick(String title);
+    }
+}
---
+++ InnerClass/src/de/dumbuya/Main.java
@@ -0,0 +1,59 @@
+package de.dumbuya;
+// The program demonstrates the use of inner classes with a Gearbox and button Class. Furthermore the use of anonymous classes is demonstrated.
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Button btnPrint = new Button("Print");
+
+    public static void main(String[] args) {
+       /* Gearbox mcLaren = new Gearbox(6);
+        mcLaren.addGear(2, 10.6);
+        mcLaren.addGear(3, 15.9);
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(1);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(1000));
+        mcLaren.changeGear(2);
+        System.out.println(mcLaren.wheelSpeed(3000));
+        mcLaren.operateClutch(true);
+        mcLaren.changeGear(3);
+        mcLaren.operateClutch(false);
+        System.out.println(mcLaren.wheelSpeed(6000)) */
+
+        /*class ClickListener implements Button.OnClickListener {
+            public ClickListener() {
+                System.out.println("I've been attached");
+            }
+
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        }
+        btnPrint.setOnClickListener(new ClickListener())*/
+        btnPrint.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(String title) {
+                System.out.println(title + " was clicked");
+            }
+        });
+        listen();
+    }
+
+    private static void listen() {
+        boolean quit = false;
+        while (!quit) {
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+            switch (choice) {
+                case 0:
+                    quit = true;
+                    break;
+
+                case 1:
+                    btnPrint.onclick();
+            }
+        }
+    }
+}
