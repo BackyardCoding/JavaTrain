91f9a0328b410d48df0b93dd81c7c684727af75d
mdumbu <moses@dumbuya.de>
2021-01-22 07:45:46 +0100

    This program is demonstrating the use of generic classes

---
+++ GenericsClass/src/de/dumbuya/BaseballPlayer.java
@@ -0,0 +1,7 @@
+package de.dumbuya;
+
+public class BaseballPlayer extends Player{
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+}
---
+++ GenericsClass/src/de/dumbuya/FootballPlayer.java
@@ -0,0 +1,7 @@
+package de.dumbuya;
+
+public class FootballPlayer extends Player{
+    public FootballPlayer(String name) {
+        super(name);
+    }
+}
