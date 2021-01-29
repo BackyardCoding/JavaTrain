a6a15af3cea2a2011a3eb12ffcbab26ac8ebbd5e
mdumbu <moses@dumbuya.de>
2021-01-29 01:56:08 +0100

    Commit the missing files of previous committed Static Program

--- Static/src/de/dumbuya/Main.java
+++ Static/src/de/dumbuya/Main.java
@@ -6,7 +6,7 @@ public class Main {
      */
     public static int multiplier = 7;
     public static void main(String[] args) {
-        /*
+
         StaticTest firstInstance = new StaticTest("1st Instance");
         System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
 
@@ -15,7 +15,7 @@ public class Main {
 
         StaticTest thirdInstance = new StaticTest("3nd instance");
         System.out.println(thirdInstance.getName() + " is instance number " + StaticTest.getNumInstances());
-         */
+
         int answer = multiply(6);
         System.out.println("The answer is " + answer);
         System.out.println("Multiplier is " + multiplier);
